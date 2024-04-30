b99845727ac0f4ee147987ad5d36b5d581508e7b
Julio <a22julioaa@iessanclemente.net>
2024-04-30 07:59:48 +0200

    Cambios db

--- legal_code/views.py
+++ legal_code/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-from usuarios.models import Profesional, Especialidad
 
-def homepage(request):
-    lista_especialidades = Especialidad.objects.all()
-    lista_profesionales = Profesional.objects.all()
-        
-    return render(request, 'homepage.html', {'lista_especialidades': lista_especialidades, 
-                                             'lista_profesionales': lista_profesionales})  
-    
-def sobre_nosotros(request):
-    return render(request, 'sobre_nosotros.html', {})   
+
 
