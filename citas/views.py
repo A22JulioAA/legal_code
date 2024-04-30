2b030221afbfef88313542c120c3443945b61338
Julio <a22julioaa@iessanclemente.net>
2024-04-30 11:23:47 +0200

    Detalles de estilos en paginas de login y register

--- citas/views.py
+++ citas/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Cita
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+@login_required
 def citas_principal(request):
     # Obtenemos todas las citas asociadas al profesional 
     citas = Cita.objects.all()
