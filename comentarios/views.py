9fae047aea248ef9a9e5691a359a9d3022c55338
a22julioaa <a22julioaa@iessanclemente.net>
2024-05-20 13:12:04 +0200

    Modificaciones en registro y login

--- comentarios/views.py
+++ comentarios/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 
+#TODO: Implementar comentarios. No hay nada hecho.
+
+
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+#TODO: Implementar la opción de modificar el perfil.
+@login_required
 def profile(request):
     data = {
         
     }
     
     return render(request, 'profile.html', data)
+
+# @login_required
+# def modificar_perfil(request):
+#     if request.method == 'POST':
+        
 
 @login_required
 def eliminar_perfil(request):
